3c6bd444c7d568816fcb9b3a5f4b88899a17033f
dilsadmohammed4 <dilsadmohammed4@gmail.com>
2023-10-10 00:50:14 +0530

    10-10-2023

---
+++ 17-React-All Concept/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Component1 } from "./components/usecontextdemo/with-context/Component1";
+
+const App = () => {
+  return (
+    <>
+      {/* //Use Context */}
+      <Component1 />
+    </>
+  );
+};
+
+export default App;
